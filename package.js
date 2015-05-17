7db4f7fef659c6da348ec3b701ca041b69805d9f
Charleno Pires <charleno@gmail.com>
2015-05-16 23:32:38 -0300

    Preparing compiler stylus with kouto swiss

--- package.js
+++ package.js
@@ -9,11 +9,30 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom(['METEOR@0.9.0', 'METEOR@1.0', 'METEOR@1.1.0.2']);
   api.use('stylus');
+  console.log(api);
+});
+
+Package._transitional_registerBuildPlugin({
+  name: 'compileStylus',
+  use: [],
+  sources: [
+    'plugin/compile-stylus.js'
+  ],
+  npmDependencies: {
+    stylus: '0.50.0',
+    kouto-swiss: '0.11.11'
+  }
 });
 
 Package.onTest(function(api) {
-  api.use(['tinytest','girassolbit:meteor-kouto-swiss']);
-  api.addFiles('kouto-swiss-tests.js', 'client');
+  api.use(['tinytest','girassolbit:meteor-kouto-swiss', 'test-helpers']);
+  api.add_files([
+    'tests/import.styl',
+    'tests/relative.import.styl',
+    'tests/absolute.import.styl',
+    'tests/koutoswiss_.styl',
+    'tests/tinytest.js'
+  ],'client');
 });
 
 Npm.depends({
